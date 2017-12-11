212a0d2b82c8f7c8be2c2e98a30c06569264d30e
Amy Jacobs <amelia.b.jacobs@gmail.com>
2017-12-10 18:22:33 -0800

    Add additional style of name

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,29 @@
 charArray = [];
 
 function nameGen() {
-  syl1Array = ["Bar","Lay", "Beth", "Bek","Bel", "Bal", "Bes", "Bys","Byr", "Ber", "Besh", "Cyr", "Col", "Der", "Dyr", "Dir", "Dor", "Dol", "Dew", "Des", "Det", "Dek", "Del", "Den", "Dem", "Dek","Ek","Er","Et","El","Ed","Es","Ex","Ew","Fer", "Fir", "Fes", "For", "Fol", "Fel", "Fen",];
+  syl1Array = ["Bar","Lay", "Beth", "Bek","Bel", "Bal", "Bes", "Bys","Byr", "Ber", "Besh", "Cyr", "Col", "Der", "Dyr", "Dir", "Dor", "Dol", "Dew", "Des", "Det", "Dek", "Del", "Den", "Dem", "Dek","Ek","Er","Et","El","Ed","Es","Ex","Ew","Fer", "Fir", "Fes", "For", "Fol", "Fel", "Fen","Gir", "Gor", "Gon", "Gow", "Ges", "Gel", "Hyl", "Hyr", "Hel", "Hes", "Jyr", "Jor", "Jeth", "Jaer", "Kir","Kol", "Kyr", "Kin", "Kith", "Lyr", "Lor", "Lil", "Lel", "Lith", "Lis", "Lyn", "Lyl", "Les", "Mel", "Mer", "Mir", "Myr", "Nel", "Nil", "Nis", "Nyr", "Nyl", "Naer", "Nar", "Ner", "Neth", "Nir", "Ryn", "Ryth", "Ran", "Rin", "Ril", "Ris", "Ser", "Sir", "Syr","Syth", "Syl", "Sil", "Sher", "Shes", "Shyr", "Shyl", "Shaer", "Til", "Tyl", "Tyr", "Tor", "Torg", "Vyr", "Vyl", "Vier", "Vir", "Vor", "Vyth", "Val", "Vel", "Vol", "Vith", "Ven", "Wyr", "Wir"];
   syl2Array = ["ran", "car","al", "il","el","as","ter","sen","en","ib","or","en", "is","ol","rol","len","on"]
-  surName1Array = ["Light","Iron","Steel","Wood","Leaf","Gold", "Gem", "Dust","Dark","Red","Dawn", "Night","Dusk","Cold","Ice", "Flame", "Wood", "Bright", "Sky","Storm","Hail","Mist","Hawk", "Eagle","Rain", "Willow","Reed", "Ash",];
+  surName1Array = ["Light","Iron","Steel","Wood","Leaf","Gold", "Gem", "Dust","Dark","Red","Dawn", "Night","Dusk","Cold","Ice", "Flame", "Wood", "Bright", "Sky","Storm","Hail","Mist","Hawk", "Eagle","Rain", "Willow","Reed", "Ash","Bear", "Dragon",];
   surName2Array = ["blade","sword","glow", "flower", "fire", "eye","soul","run","sun", "blood","star","heart","shadow","strider","wing","cloud","blaze","bow","song","singer",];
+
+  epithArray = ["the Red", "the Brave", "the Good", "the Loud", "the Silent", "the Wise", "the Pale", "the Cursed","the Ugly", "the Beautiful", "the False", "the Wretched", "the Hound", "the Dragon", "the Beast", "the Thief", "the Scrawny", "the Hawk", "the Rat", "the Spider", "the Merciful", "the Lamb", "the Cold", "the Fox", "the Owl", "the Angry", "the Victorious", "the Bear", "the Risen", "the Phoenix", "the Loyal", "the Shield", "the Swift", "the White", "the Grey", "the Dark", "the Shadow", "the Bloody"];
+
+  nameStyleArray = ["Epithet", "Surname"];
+  var nameStyleNum = Math.floor(Math.random() * nameStyleArray.length);
+  var nameStyle = nameStyleArray[nameStyleNum];
+
+  if (nameStyle === "Surname") {
+    var surNameNum1 = Math.floor(Math.random() * surName1Array.length);
+    var surNameNum2 = Math.floor(Math.random() * surName2Array.length);
+    var surName = surName1Array[surNameNum1] + surName2Array[surNameNum2];
+  } else if (nameStyle === "Epithet"){
+    epithNum = Math.floor(Math.random() *  epithArray.length);
+    var surName = epithArray[epithNum];
+  }
+
   var syl1Num = Math.floor(Math.random() * syl1Array.length);
   var syl2Num = Math.floor(Math.random() * syl2Array.length);
-
-  var surNameNum1 = Math.floor(Math.random() * surName1Array.length);
-  var surNameNum2 = Math.floor(Math.random() * surName2Array.length);
-
   var firstName = syl1Array[syl1Num] + syl2Array[syl2Num];
-  var surName = surName1Array[surNameNum1] + surName2Array[surNameNum2];
   var name = firstName + " " + surName;
   return name;
 }
@@ -40,7 +51,7 @@ function personalityGen() {
 }
 
 function jobGen() {
-  jobArray = ["Wizard","Cleric", "Knight", "Ranger", "Thief", "Warlock", "Merchant", "Shephard","Blacksmith", "Scribe", "Tailor","Carpenter","Squire","Baker","Watchman","Druid", "Monk","Hunter", "Shaman"]
+  jobArray = ["Wizard","Cleric", "Knight", "Ranger", "Thief", "Warlock", "Merchant", "Shephard","Blacksmith", "Scribe", "Tailor","Carpenter","Squire","Baker","Watchman","Druid", "Monk","Hunter", "Shaman", "Assasin", "Healer",]
   var jobNum = Math.floor(Math.random() * jobArray.length);
   var job = jobArray[jobNum];
   return job;
